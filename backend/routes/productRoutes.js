7fd6261b215c77485274dfd663458b4aa6c6cadd
Sikander Merchant <sikander.merchant@mstratdigital.com>
2023-08-14 22:27:46 +0530

    fetching products from DB and Errorhandling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,18 +9,26 @@ import Product from '../models/productModel.js';
 router.get('/', asyncHandler(async (req, res) => { 
   const products = await Product.find({});
   if(products) {
-    res.status(200).json(products);
+    return res.status(200).json(products);
   } else {
-    res.status(404);
-    throw new Error('Products not found');
+    res.status(404)
+    throw new Error('Resource not found'); 
   }
-}));
+})
+);
 
 
 router.get('/:id', asyncHandler(async(req, res) => { 
   // const product = products.find((p) => p._id === req.params.id);
   const product = await Product.findById(req.params.id);
-  res.json(product);
-}));
+  if (product) {
+    return res.status(200).json(product);
+  }
+  else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+})
+);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import productRoutes from './routes/productRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 
 connectDB(); //connect to MongoDB
@@ -12,6 +13,8 @@ app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server at http://localhost:${port}`)
